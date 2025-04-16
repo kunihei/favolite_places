f05d5e6f3b6017714149f29621ee967711eba540
SI-KunihisaShohei <127285951+SI-KunihisaShohei@users.noreply.github.com>
2025-04-16 20:43:12 +0900

    会社で勉強した最新のコードを取り込みたい (#2)
    * 241までの動画を追加
    
    * 242までの動画の内容を追加
    
    ---------
    
    Co-authored-by: kunihisa <>

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:favorite_places/models/place.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super([]);
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [
+      newPlace,
+      ...state,
+    ];
+  }
+}
+
+final usesrPlacesProvider = StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+  (ref) => UserPlacesNotifier(),
+);
